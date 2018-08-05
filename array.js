af0700d5d5c031c2ba94bf1f413a058677900310
HoangChien <chienhd@hellomedia.vn>
2018-08-05 09:22:38 +0700

    array

--- array.js
+++ array.js
@@ -1,18 +1,25 @@
 var cars = ['Saab', 'Volvo', 'BMW'];
-console.log(cars.valueOf());
+// console.log(cars.valueOf());
 
-for (var i = 0; i < cars.length; i++) {
-    console.log(cars[i]);
-}
+// for (var i = 0; i < cars.length; i++) {
+//     console.log(cars[i]);
+// }
 
-console.log(cars.join(" and "));
+// console.log(cars.join(" and "));
 
-cars.push("Honda");
-cars.push("Yamaha");
-cars.push("Toyota");
-console.log(cars.valueOf());
+// cars.push("Honda");
+// cars.push("Yamaha");
+// cars.push("Toyota");
+// console.log(cars.valueOf());
 
-while (cars.length > 0) {
-    cars.pop();
-    console.log(cars.valueOf());
-}
+// while (cars.length > 0) {
+//     cars.pop();
+//     console.log(cars.valueOf());
+// }
+
+// while (cars.length > 0) {
+//     cars.shift();
+//     console.log(cars.valueOf());
+// }
+
+cars.push("Thaco");
